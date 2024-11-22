31be0fadf27008d91b788d6ebdba5eeb5b8d67d5
Littlestar0508 <rist9860@gmail.com>
2024-11-23 01:24:53 +0900

    함수 분리 1차(이벤트 핸들러에서 배경색 변경,이미지 변경, 제목 변경 함수 분리)

--- mission02/js/main.js
+++ mission02/js/main.js
@@ -10,14 +10,37 @@
 
 const container = document.querySelector('.container');
 const nav = document.querySelector('.nav');
-const imgList = document.querySelectorAll('ul > li');
-const title = document.querySelector('.nickName');
-const visual = document.querySelector('.visual img');
 
+//배경색 바꾸기 함수
+const setBgColor = (colorA, colorB = '#000') => {
+  const body = document.body;
+
+  body.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
+};
+
+// 이미지 변경 함수
+const setImage = ({ alt, name }) => {
+  const visual = document.querySelector('.visual img');
+
+  visual.alt = alt;
+  visual.src = `./assets/${name}.jpeg`;
+};
+
+// 제목 변경 함수
+const setNameText = (name) => {
+  const title = document.querySelector('.nickName');
+
+  title.textContent = name;
+};
+
+// 클릭 이벤트 핸들러
 const handleClick = (e) => {
+  const imgList = document.querySelectorAll('ul > li');
+
   // 흰색 테두리 바꾸기
   const li = e.target.closest('li');
 
+  // 만약 li의 영역을 빠져 나갔다면 이벤트 실행X
   if (!li) return;
 
   imgList.forEach((v) => {
@@ -26,12 +49,20 @@ const handleClick = (e) => {
 
   li.classList.add('is-active');
 
-  // 사진 바꾸기
+  // data-index의 값과 그에 해당하는 data를 index와 dataSet에 저장
   const index = li.dataset.index;
-  title.textContent = data[index - 1].name;
-  visual.alt = data[index - 1].alt;
-  visual.src = `./assets/${data[index - 1].name}.jpeg`;
-  document.body.style.background = `linear-gradient(to bottom,${data[index - 1].color[0]},${data[index - 1].color[1]})`;
+
+  const dataSet = data[index - 1];
+
+  // 제목 변경
+  setNameText(dataSet.name);
+
+  // 사진 바꾸기
+
+  setImage(dataSet);
+
+  // 배경색 바꾸기
+  setBgColor(dataSet.color[0], dataSet.color[1]);
 };
 
 nav.addEventListener('click', handleClick);
