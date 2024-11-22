bc0de8bfeb630e1f53fb0dd0cb4a2cbac13356dc
Littlestar0508 <rist9860@gmail.com>
2024-11-23 00:51:51 +0900

    이미지 변경과 배경색 변경 구현(함수분리X)

--- mission02/js/main.js
+++ mission02/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,30 @@
 
 */
 
+const container = document.querySelector('.container');
+const nav = document.querySelector('.nav');
+const imgList = document.querySelectorAll('ul > li');
+const title = document.querySelector('.nickName');
+const visual = document.querySelector('.visual img');
 
+const handleClick = (e) => {
+  // 흰색 테두리 바꾸기
+  const li = e.target.closest('li');
 
+  if (!li) return;
 
+  imgList.forEach((v) => {
+    v.classList.remove('is-active');
+  });
 
+  li.classList.add('is-active');
 
+  // 사진 바꾸기
+  const index = li.dataset.index;
+  title.textContent = data[index - 1].name;
+  visual.alt = data[index - 1].alt;
+  visual.src = `./assets/${data[index - 1].name}.jpeg`;
+  document.body.style.background = `linear-gradient(to bottom,${data[index - 1].color[0]},${data[index - 1].color[1]})`;
+};
 
-
-
-
-
-
-
-
+nav.addEventListener('click', handleClick);
