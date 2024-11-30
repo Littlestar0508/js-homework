a3252d77498d292ae43c791dc7b59e59ca13c28c
Littlestar0508 <rist9860@gmail.com>
2024-12-01 01:37:25 +0900

    swiper 경진대회 참가

---
+++ Swiper/src/main.js
@@ -0,0 +1,28 @@
+const colorData = [
+  '#0D0D0D',
+  '#98AADA',
+  '#B6CDBB',
+  '#9996B1',
+  '#CEA7A0',
+  '#B78EC6',
+  '#B7B5CD',
+  '#F2B705',
+];
+
+const swiper = new Swiper('.swiper', {
+  nested: true,
+  effect: 'cards',
+  normalizeSlideIndex: true,
+  direction: 'horizontal',
+  autoplay: {
+    delay: 3000,
+  },
+
+  on: {
+    slideChangeTransitionEnd: function () {
+      const container = document.querySelector('.container');
+      container.style.backgroundImage = `url('../../assets/bgImg${this.activeIndex}.jpg')`;
+      document.body.style.backgroundColor = colorData[this.activeIndex];
+    },
+  },
+});
